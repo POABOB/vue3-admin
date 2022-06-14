8817a95f4f96ca8c44a57bd9e42a00610441c6dd
POABOB <38785340+POABOB@users.noreply.github.com>
2022-06-14 23:53:00 +0800

    style(vue.config.js): add @/components to alias

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const path = require(path)
+
+module.exports = {
+  // 將路徑components簡化
+  // components/HelloWorld.vue or @/components/HelloWorld.vue
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", path.resolve(__dirname, "src"))
+      .set("components", "@/components")
+  }
+}
